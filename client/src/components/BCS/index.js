d551a88e3492623565ce5211d2c6a2c2fb6e5db4
HWMooo <58176899+HWMooo@users.noreply.github.com>
2022-05-11 21:05:42 +0100

    Update index.js

--- client/src/components/BCS/index.js
+++ client/src/components/BCS/index.js
@@ -53,7 +53,7 @@ function BCS(props) {
         navigator.mediaDevices.enumerateDevices()
             .then(function (devices) {
                 devices.forEach(function (device) {
-                warn(device.kind + ": " + device.label +
+                console.log(device.kind + ": " + device.label +
                     " id = " + device.deviceId);
             });
         })
