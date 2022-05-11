8dbedd89b173d1f9d16bf4a92758a029e40d3336
HWMooo <58176899+HWMooo@users.noreply.github.com>
2022-05-11 16:43:46 +0100

    Update index.js

--- client/src/components/BCS/index.js
+++ client/src/components/BCS/index.js
@@ -102,7 +102,7 @@ function BCS() {
             Quagga.start();
             const track = Quagga.CameraAccess.getActiveTrack(); sleep(5000); 
             var capabilities = track.getCapabilities(); 
-            track.applyConstraints({ advanced: [{zoom: capabilities.zoom.max}]})
+            track.applyConstraints({ advanced: [{zoom: 2.5}]})
                 .catch(e => console.log(e));
             setScannerRunning(true)
             document.querySelector('canvas').style.display = "inline";
