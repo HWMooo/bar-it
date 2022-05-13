a380bc29fc2e5e5794f10148f1918dcb2ba405a4
HWMooo <58176899+HWMooo@users.noreply.github.com>
2022-05-13 09:31:24 +0100

    td

--- client/src/components/NEWSCANNER/index.js
+++ client/src/components/NEWSCANNER/index.js
@@ -163,7 +163,7 @@ function NEWSCANNER() {
                     width: 680,
                     height: 420,
                     facingMode:"environment",
-                    //deviceId: "" + cameraTypes[cameraTypes.length-2]
+                    deviceId: ameraTypes[cameraTypes.length-2]
 
 
                 },
